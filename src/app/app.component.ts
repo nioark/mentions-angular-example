b4223ed3011d1d2ee6757c942dc54d9cc3a1d944
Nicolas Rausch <122921640+nioark@users.noreply.github.com>
2024-08-05 17:42:47 -0300

    Reworked logic, added events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import {OverlayModule} from '@angular/cdk/overlay';
+
 
 interface User {
   id: string
   display: string
 }
 
+interface Mention {
+  user: User | undefined,
+  text: string,
+  index: number,
+  ended: boolean,
+  str_start_index: number,
+  str_end_index: number
+}
+
+interface MentionCase {
+  text: string,
+  type: string,
+  start_index: number,
+  end_index: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, OverlayModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'mentions';
 
   _message : string = '';
@@ -27,9 +45,35 @@ export class AppComponent {
     {id : '1', display : 'John'},
     {id : '2', display : 'Jane'},
     {id : '3', display : 'Bob'},
+    {id : '4', display : 'Joao'},
+    {id : '5', display : 'Jorge'}
   ]
 
-  constructor(private sanitizer: DomSanitizer) {}
+  showMatches = false;
+
+  currentMatches : User[] = [];
+  selectedMatchIndex = 0;
+
+  mentionsProcessed : Mention[] = [];
+  currentMention : Mention | undefined;
+
+  @ViewChild('textarea') textarea: ElementRef<HTMLTextAreaElement> | undefined;
+
+  constructor(private sanitizer: DomSanitizer, private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    let caret_pos_changing_events = ['keypress', 'mousedown', 'touchstart', 'input', 'paste', 'cut', 'mousemove', 'select', 'selectstart']
+    console.log(this.textarea)
+    for (const event of caret_pos_changing_events) {
+      this.textarea?.nativeElement.addEventListener(event, () => {
+        this.onCaretChange();
+      })
+    }
+  }
+
+  getJson() : string {
+    return JSON.stringify(this.mentionsProcessed)
+  }
 
   get message() {
     return this._message;
@@ -39,7 +83,7 @@ export class AppComponent {
   }
 
   get highlights() : SafeHtml {
-    let mentions = this.convertStringToMentionDict(this.message)
+    let mentions = this.getMentionDictToHighlight(this.message)
 
     let html = '';
 
@@ -54,23 +98,43 @@ export class AppComponent {
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
-  convertStringToMentionDict(inputString: string): { text: string, type: string }[] {
-    const mentionRegex = /@\w+/g;
-    const textParts = inputString.split(mentionRegex);
-    const mentionParts = inputString.match(mentionRegex) || [];
+  getMentionDictToHighlight(inputString: string): MentionCase[] {
+    return this.getAllMentionCases(inputString)
+  }
+
+  getMentionDictFromNames(inputString: string, names : string[]): { text: string, type: string }[] {
+    const regex = new RegExp(names.map(name => name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|"));
+    console.log("Regex for names: ", regex);
+
+    return this._getMentionDict(inputString, regex);
+  }
+
+  getAllMentionCases(inputString: string): MentionCase[] {
+    const mentionRegex = /@\w+(?:\s\w+)*/g;
+
+    return this._getMentionDict(inputString, mentionRegex);
+  }
+
+  _getMentionDict(inputString: string, regex: RegExp): MentionCase[] {
+    const textParts = inputString.split(regex);
+    const mentionParts = inputString.match(regex) || [];
   
-    const result: { text: string, type: string }[] = [];
+    const result: MentionCase[] = [];
+
+    let currentIndex = 0;
   
     for (let i = 0; i < textParts.length; i++) {
       const textPart = textParts[i];
       const mentionPart = mentionParts[i];
   
       if (textPart) {
-        result.push({ text: textPart, type: "normal" });
+        result.push({ text: textPart, type: "normal", start_index: currentIndex, end_index: currentIndex + textPart.length });
+        currentIndex += textPart.length;
       }
   
       if (mentionPart) {
-        result.push({ text: mentionPart, type: "mention" });
+        result.push({ text: mentionPart, type: "mention", start_index: currentIndex, end_index: currentIndex + mentionPart.length });
+        currentIndex += textPart.length;
       }
     }
 
@@ -85,44 +149,182 @@ export class AppComponent {
     return '<strong style="font-weight: inherit; background-color: rgb(206, 228, 229);">' + text + '</strong>';
   }
 
-  _get_possible_match(name: string) : User | undefined {
+  _getPossibleMatch(name: string) : User[] | undefined {
+    let users : User[] = [];
+
     for (const user of this.users_to_mention) {
       let display_name = user.display.toLocaleLowerCase()
       if (display_name.startsWith(name)) {
-        return user;
+        users.push(user);
       }
+    }
+
+    if (users.length > 0) {
+      return users
     }
 
     return undefined
   }
 
-  on_a_match = false;
-  possible_match : User | undefined;
+  getCurrentMention() : Mention | undefined {
+    const mentions = this.getAllMentionCases(this.message).filter(mention => mention.type == "mention");
 
-  onMessageChange(message: string) {
-    this.message = message;
+    let caret_position : number = this.textarea ? this.textarea.nativeElement.selectionStart : 0;
 
-    const mentions = this.convertStringToMentionDict(message);
-    const lastMention = mentions[mentions.length - 1];
-    if (lastMention.type == "mention") {
-      let name = lastMention.text.substring(1).toLocaleLowerCase();
-      this.possible_match = this._get_possible_match(name)
-      this.on_a_match = true;
+    let mention_index = -1
+    let mentionFound : MentionCase | undefined = undefined
+
+    mentions.forEach((mention, index) => {
+      if (caret_position >= mention.start_index && caret_position <= mention.end_index + 1) {
+        mention_index = index
+        mentionFound = mention
+      }
+    }) 
+
+    console.log(mentions)
+
+    if (mention_index != -1 && mentionFound) {
+      if (this.mentionsProcessed[mention_index]) {
+        return this.mentionsProcessed[mention_index]
+      }
+
+      let currentMention = mentionFound as MentionCase
+
+      let name = currentMention.text.substring(1).toLocaleLowerCase();
+
+      let possible_matches = this._getPossibleMatch(name);
+      let possible_match = possible_matches ? possible_matches[0] : undefined;
+
+      this.currentMatches = possible_matches || [];
+
+      let mention : Mention = {
+        text: currentMention.text,
+        user: possible_match,
+        index: mention_index,
+        str_start_index: currentMention.start_index,
+        str_end_index: currentMention.end_index,
+        ended: false
+      }
+
+      return mention
+    } 
+
+    return undefined
+  }
+
+
+  onChangedMessage($event: string) {
+    this.message = $event;
+    this._process();
+  }
+
+  onCaretChange() {
+    this._process();
+  }
+
+  _process(){
+    let mention = this.getCurrentMention();
+    this.currentMention = mention;
+
+    if (this.currentMention && !this.currentMention.ended) {
+      this.showMatches = true;
     } else {
-      this.possible_match = undefined
-      this.on_a_match = false
+      this.showMatches = false
     }
   }
 
+  onSelectEvent() {
+    if (!this.currentMention) return
+
+    if (this.currentMention.ended) return
+
+    if (this.currentMatches.length > 0) {
+      let found_user = this.currentMatches[this.selectedMatchIndex];
+      if (found_user){
+        this.currentMention.text = '@' + found_user.display
+        this.currentMention.str_end_index = this.currentMention.str_end_index + this.currentMention.text.length + 1
+        this.currentMention.user = found_user;
+      }
+    }
+
+    this.currentMention.ended = true;
+
+    if (!this.mentionsProcessed[this.currentMention.index]) {
+      this.mentionsProcessed.push(this.currentMention);
+    } else {
+      this.mentionsProcessed[this.currentMention.index] = this.currentMention
+    }
+
+    if (this.currentMention.user) {
+      const start_index = this.currentMention.str_start_index + 1;
+      const end_index = this.currentMention.str_end_index;
+      const before = this.message.substring(0, start_index);
+      const after = this.message.substring(end_index, this.message.length);
+      this.message = before + this.currentMention.user.display + after;
+      this.changeDetectorRef.detectChanges();
+      this._setCursorPos(end_index);
+    }
+    
+    this.selectedMatchIndex = 0;
+
+  }
+
+  onChooseEvent(indexTo: number) {
+
+    if (this.currentMatches.length == 0) return;
+
+    this.selectedMatchIndex += indexTo;
+    this.selectedMatchIndex = Math.max(0, Math.min(this.selectedMatchIndex, this.currentMatches.length - 1));
+
+    // this.mentionsProcessed[indexTo] = mention;
+  }
+  _setCursorPos(index: number) {
+    if (this.textarea) {
+      this.textarea.nativeElement.selectionStart = index;
+      this.textarea.nativeElement.selectionEnd = index;
+    }
+  }
+
+  onRemoveEvent() {
+    if (!this.currentMention) return
+
+    console.log(this.currentMention)
+
+    this.mentionsProcessed.splice(this.currentMention.index, 1);
+    this.showMatches = false;
+    if (this.currentMention.user) {
+      const start_index = this.currentMention.str_start_index + 1;
+      const end_index = this.currentMention.str_end_index;
+      const before = this.message.substring(0, start_index);
+      const after = this.message.substring(end_index, this.message.length);
+
+      this._setCursorPos(start_index);
+      this.message = before + after;
+      this.changeDetectorRef.detectChanges();
+    }
+    
+  }
+
   onKeydown(event: KeyboardEvent) {
-    console.log(event);
 
-    if (!this.on_a_match && !this.possible_match) return 
+    this._process();
 
-    if (event.key == "Enter" || event.key == "Space") {
+    if (!this.currentMention) return;
+    
+    //Selected the mention event
+    if (event.code == "Space") {
+      this.onSelectEvent();
+    }
+
+    if (event.code == "ArrowUp" || event.code == "ArrowDown") {
       event.preventDefault();
-      
-      console.log("Selected match: ", this.possible_match);
+      let indexTo = event.code == "ArrowUp" ? -1 : 1;
+
+      this.onChooseEvent(indexTo);
+    }
+
+    if (event.code == "Backspace") {
+      this.onRemoveEvent();
     }
   }
 }
